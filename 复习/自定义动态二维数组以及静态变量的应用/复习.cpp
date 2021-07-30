86f6b6e84532f2272a1f9c3dcdf98fc5858041a8
Dixon <1536635817@qq.com>
2021-07-30 23:11:44 +0100

    review

---
+++ 复习/自定义动态二维数组以及静态变量的应用/复习.cpp
@@ -0,0 +1,51 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+double **create_matrix(int, int);
+void disp_matrix(double**,int ,int);
+int  increase();
+int main()
+{
+	double **matrix1=create_matrix(2,3);
+	disp_matrix(matrix1, 2, 3);
+	double** matrix2 = create_matrix(2, 3);
+	disp_matrix(matrix2, 2, 3);
+	return 0;
+}
+double **create_matrix(int m, int n)
+{
+	double** matrix = new double *[m];
+	for (int i = 0; i < m; i++)
+	{
+		matrix[i] = new double [n];
+	}
+	for (int i = 0; i < m; i++)
+	{
+		cout << "Row " << i + 1 << ": ";
+		for (int j = 0; j < n; j++)
+		{
+			cin >> matrix[i][j];
+		}
+	}
+	return matrix;
+}
+void disp_matrix(double **matrix,int m,int n)
+{
+	int j = increase();
+	cout << "Matrix " <<j<<endl;
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << matrix[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+int increase()
+{
+	static int i = 0;
+	i++;
+	return i;
+}
