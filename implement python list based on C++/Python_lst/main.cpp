3dcf23006be1d4844ff9163ccfbbf0d960f77c61
Mirage <yliu431@sheffield.ac.uk>
2021-09-09 16:54:40 +0100

    add a restart

--- implement python list based on C++/Python_lst/main.cpp
+++ implement python list based on C++/Python_lst/main.cpp
@@ -9,16 +9,14 @@ int main()
 {
 	using namespace std;
 	list a;
-	a.append(0);
+	a.append(3);
 	a.append(1);
-	list b;
-	b.append(1);
-	b.append(2);
-	a.insert(1, 2);
-	a.insert(1, "sb");
-	a.insert(1,b);
-	a.extend(b);
-	cout << a;
+	a.append(6);
+	a.append(2);
+	a.append(6);
+	cout << "Befor sorting:" << a << endl;
+	a.sort();
+	cout << "After sorting:" << a << endl;
 	return 0;
 
 }
