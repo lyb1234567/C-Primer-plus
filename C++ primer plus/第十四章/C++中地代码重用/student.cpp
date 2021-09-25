f8540210f40be956ecce0fc7ad32c790fc3b5fd9
Dixon <yliu431@sheffield.ac.uk>
2021-09-25 21:25:25 +0100

    fix

---
+++ C++ primer plus/第十四章/C++中地代码重用/student.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+struct A
+{
+    A(int) { }      // converting constructor
+    A(int, int) { } // converting constructor (C++11)
+    operator bool() const { return true; }
+};
+
+struct B
+{
+    explicit B(int) { }
+    explicit B(int a, int b) { cout << a << endl << b; }
+    explicit operator bool() const { return true; }
+};
+
+int main()
+{
+
+//  B b1 = 1;      // error: copy-initialization does not consider B::B(int)
+    B b2(2);       // OK: direct-initialization selects B::B(int)
+    B b3{ 4, 5 };   // OK: direct-list-initialization selects B::B(int, int)
+//  B b4 = {4, 5}; // error: copy-list-initialization does not consider B::B(int,int)
+    B b5 = (B)1;   // OK: explicit cast performs static_cast
+    if (b2);      // OK: B::operator bool()
+//  bool nb1 = b2; // error: copy-initialization does not consider B::operator bool()
+    bool nb2 = static_cast<bool>(b2); // OK: static_cast performs direct-initialization
+}
